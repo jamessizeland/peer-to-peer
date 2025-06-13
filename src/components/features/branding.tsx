2e5e9bec66efb8339823ab3feae56dd44ba40424
James Sizeland <jamessizeland@gmail.com>
2025-06-13 18:04:56 +0100

    update branding style

--- src/components/features/branding.tsx
+++ src/components/features/branding.tsx
@@ -1,7 +1,7 @@
 const Branding = () => {
   return (
-    <div className="text-2xl mb-1 font-bold uppercase align-middle text-white mt-auto">
-      Cups
+    <div className="text-2xl mb-1 uppercase font-bold align-middle mt-auto">
+      <span className="text-white">Cups</span>
     </div>
   );
 };
